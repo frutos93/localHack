ef04099ca77e23c768f55985fdb16b968c4f2cd0
Carlos Frutos <cefg93@gmail.com>
2016-12-03 12:30:08 -0600

    add a iterator controller

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 var tweets = document.getElementsByClassName('js-tweet-text tweet-text');
-var timer = setInterval(changeTexts, 1000);
+var timer = setInterval(changeTexts, 1500);
+var lastTweet = 0;
+var lastIter;
 
 function changeTexts() {
-    for (var i = 0, l = tweets.length; i < l; i++) {
+    for (var i = lastTweet, l = tweets.length; i < l; i++) {
         tweets[i].innerText = 'Tu mama';
+        lastTweet = i;
     }
 }
